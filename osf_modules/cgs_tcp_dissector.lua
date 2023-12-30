4f33c43d9e535d21f5924d38f14d09dbc4375df2
CarlosGS-98 <cgonzalezsanz98@gmail.com>
2023-12-30 05:48:41 +0100

    Completed the TCP signature building function

--- osf_modules/cgs_tcp_dissector.lua
+++ osf_modules/cgs_tcp_dissector.lua
@@ -14,7 +14,6 @@ local osf_tcp_dissector = {}
 
 -- Plugin constants/global variables
 CGS_OS_TCP_PROTO = CGS_OS_PROTO .. "-tcp"
-CGS_OS_TCP_STREAM_PREFIX = "osf_tcp_stream_"
 
 -- Fingerprinting protocol for TCP
 local cgs_tcp_proto = Proto(CGS_OS_TCP_PROTO, "OS Fingerprinting - TCP")
@@ -28,18 +27,31 @@ local osf_tcp_dst = Field.new("tcp.dstport")
 
 -- More specific TCP parameters
 local osf_tcp_wsize = Field.new("tcp.window_size")
+local osf_tcp_mss = Field.new("tcp.options.mss_val")
+local osf_ip_ttl = Field.new("ip.ttl")
+local osf_tcp_wscale = Field.new("tcp.options.wscale.shift")
+local osf_ip_df = Field.new("ip.flags.df")
+local osf_tcp_len = Field.new("tcp.len")
 
 -- Judging by Satori's own signatures, only the
 -- SYN and ACK flags are ever taken into account:
 
 local osf_tcp_ack = Field.new("tcp.flags.ack")
+local osf_tcp_ack_num = Field.new("tcp.ack")
 local osf_tcp_syn = Field.new("tcp.flags.syn")
-local osf_tcp_fin = Field.new("tcp.flags.fin")  -- To free entries from the lookup table whenever we're sure that a given TCP connection has ended
+local osf_tcp_fin = Field.new("tcp.flags.fin")      -- To free entries from the lookup table whenever we're sure that a given TCP connection has ended
+local osf_tcp_urgent = Field.new("tcp.flags.urg")
+local osf_tcp_flags = Field.new("tcp.flags")        -- To detect packet anomalies
 
 local osf_tcp_options = Field.new("tcp.options")
 
+-- Header parameters
+local osf_ip_header_len = Field.new("ip.hdr_len")
+local osf_ip_id = Field.new("ip.id")
+local osf_ip_length = Field.new("ip.len")
+local osf_tcp_header_len = Field.new("tcp.hdr_len")
+
 -- Extra field to store a TCP stream lookup table
-local cgs_tcp_stream_id = 0 -- By default for each capture session/reload
 local cgs_tcp_stream_table = {
     --[[
         Each entry in this table will have the following format:
@@ -80,8 +92,177 @@ local osf_tcp_xml = osf.preloadXML(OSF_SATORI_TCP)
 
 function osf_tcp_dissector.osf_build_tcp_signature(packet_data)
     -- (...)
+    --[[
+        packet_data = {
+            window_size = tonumber(tcp_wsize.value),
+            df_bit = ip_df_check.value,
+            --syn_bit = syn_check.value,
+            --ack_bit = ack_check.value,
+            tcp_header_len = tonumber(tcp_hdrlen.value),
+            ip_header_len = tonumber(ip_hdrlen.value),
+            packet_len = tonumber(tcp_len.value),
+            mss = tonumber(tcp_mss.value),
+            ttl = tonumber(ip_ttl.value),
+            window_scale = tonumber(tcp_wscale.value),
+            options = tostring(tcp_options.value)
+        }, (...)
+    ]]--
 
-    -- return (current TCP signature with p0f's format regarding TCP options)
+    -- We'll store some of the info contained inside packet_data
+    -- inside a new anonymous table so we can later concatenate its contents
+    -- (ideally with ":" as the separator).
+    --
+    -- Additionally, we'll use two additional tables to create the
+    -- whole TCP signature (in p0fv2 format):
+    -- one for storing TCP options and another one for TCP quirks.
+
+    -- Temporary table to hold the first part of our signature:
+    -- (FORMAT): "window_size:ttl:df_bit:total_header_length"
+
+    print(inspect(packet_data))
+    local final_tcp_sig = ""
+
+    local tcp_sig_first = table.concat({
+        tostring(packet_data["window_size"]),
+        tostring(packet_data["ttl"]),
+        tostring(packet_data["df_bit"] and 1 or 0),
+        tostring(tonumber(packet_data["ip_header_len"]) + tonumber(packet_data["tcp_header_len"]))
+    }, ":")
+
+    -- The second part of our signature will contain
+    -- all TCP options that have been specified
+    -- for the current packet. In order to get these,
+    -- we'll have to treat tcp.options as a ByteArray,
+    -- which enables us to traverse them in the same
+    -- exact order they were sent over the network:
+
+    local tcp_sig_second = ":"
+    local tcp_timestamp_echo = nil
+    local tcp_timestamp_reply = nil
+
+    -- The options will be a list of comma-separated values
+    if packet_data["options"] ~= nil then
+        local option_index = 0
+
+        -- Guaranteed single-pass traversal of the TCP options array
+        repeat
+            -- [NOTE]: This snippet is similar to https://github.com/xnih/satori/blob/master/satoriTCP.py,
+            -- only written in Lua and with some syntactic modifications.
+            if option_index > 0 then
+                tcp_sig_second = tcp_sig_second .. ","
+            end
+
+            print("Current byte = " .. tostring(packet_data["options"]:get_index(option_index)))
+
+            -- The current byte will be, in this case, our TCP kind
+            local cur_tcp_kind = tonumber(packet_data["options"]:get_index(option_index))
+            print("TCP Kind = " .. tostring(cur_tcp_kind))
+
+            if cur_tcp_kind == 0 then               -- End
+                tcp_sig_second = tcp_sig_second .. "E"
+                option_index = option_index + 1
+            elseif cur_tcp_kind == 1 then           -- NOP
+                tcp_sig_second = tcp_sig_second .. "N"
+                option_index = option_index + 1
+            elseif cur_tcp_kind == 2 then           -- MSS
+                -- We already have this, so we concatenate it directly --
+                tcp_sig_second = tcp_sig_second .. "M" .. tostring(packet_data["mss"])
+                option_index = option_index + 4
+            elseif cur_tcp_kind == 3 then           -- Window Scale
+                -- We already have this, so we concatenate it directly
+                tcp_sig_second = tcp_sig_second .. "W" .. tostring(packet_data["window_scale"])
+                option_index = option_index + 3
+            elseif cur_tcp_kind == 4 then           -- SACK Permitted
+                tcp_sig_second = tcp_sig_second .. "S"
+                option_index = option_index + 2
+            elseif cur_tcp_kind == 5 then           -- SACK
+                tcp_sig_second = tcp_sig_second .. "K"
+                option_index = option_index + tonumber(packet_data["options"]:get_index(option_index + 1))
+            elseif cur_tcp_kind == 6 then          -- Echo
+                tcp_sig_second = tcp_sig_second .. "J"
+                option_index = option_index + 6
+            elseif cur_tcp_kind == 7 then          -- Echo Reply
+                tcp_sig_second = tcp_sig_second .. "F"
+                option_index = option_index + 6
+            elseif cur_tcp_kind == 8 then          -- Timestamps
+                tcp_sig_second = tcp_sig_second .. "T"
+
+                -- Get timestamp data in case there's
+                -- an anomaly in the current packet:
+
+                tcp_timestamp_echo = tonumber(packet_data["options"]:uint(option_index, 4))
+                tcp_timestamp_reply = tonumber(packet_data["options"]:uint(option_index + 4, 4))
+
+                option_index = option_index + 10
+            elseif cur_tcp_kind == 9 then          -- Partial Order Connection
+                tcp_sig_second = tcp_sig_second .. "P"
+                option_index = option_index + 2
+            elseif cur_tcp_kind == 10 then         -- Partial Order Service
+                tcp_sig_second = tcp_sig_second .. "R"
+                option_index = option_index + 3
+            else
+                tcp_sig_second = tcp_sig_second .. "U"
+                option_index = option_index + 1
+            end
+
+            print("Options = \"" .. tcp_sig_second .. "\"")
+            print("Out of bounds?: " .. tostring(option_index >= packet_data["options"]:len()))
+            print("Option Index = " .. tostring(option_index) .. "; Options Length = " .. tostring(packet_data["options"]:len()))
+        until option_index >= packet_data["options"]:len()
+
+        tcp_sig_second = tcp_sig_second .. ":"
+        print("Current TCP Signature (with options) = \"" .. tostring(tcp_sig_first) .. tostring(tcp_sig_second) .. "\"")
+	else
+        tcp_sig_first = tcp_sig_first .. "*:"    -- "*" as a wildcard for searching later inside the fingerprint database
+    end
+
+    -- The third and last part of our signature
+    -- will consist of quirks that the current packet
+    -- presents, if there are any present:
+
+    local tcp_sig_third = ""
+
+    -- Heavily based off https://github.com/xnih/satori/blob/master/satoriTCP.py --
+    if (packet_data["options"] ~= nil) and (tonumber(packet_data["options"]:get_index(packet_data["options"]:len() - 1)) == 0) then
+        tcp_sig_third = tcp_sig_third .. "P"
+    end
+
+    if packet_data["ip_id"] == 0 then
+        tcp_sig_third = tcp_sig_third .. "Z"
+    end
+
+    if packet_data["ip_header_len"] > 20 then
+        tcp_sig_third = tcp_sig_third .. "I"
+    end
+
+    if (packet_data["ip_length"] - packet_data["ip_header_len"] - packet_data["tcp_header_len"]) ~= 0 then
+        tcp_sig_third = tcp_sig_third .. "D"
+    end
+
+    if packet_data["urg_bit"] then
+        tcp_sig_third = tcp_sig_third .. "U"
+    end
+
+    if (packet_data["syn_check"] or (packet_data["syn_check"] and packet_data["ack_check"])) and (packet_data["ack_num"] ~= 0) then
+        tcp_sig_third = tcp_sig_third .. "A"
+    end
+
+    local timestamp_delta = tcp_timestamp_reply - tcp_timestamp_echo
+
+    if (packet_data["syn_flag"] and timestamp_delta ~= 0) or (packet_data["syn_flag"] and packet_data["ack_flag"] and tcp_timestamp_echo ~= nil and tcp_timestamp_reply ~= nil) then
+        tcp_sig_third = tcp_sig_third .. "T"
+    end
+
+    -- Lua doesn't support bitwise operators until Lua 5.3
+    if (bit32.band(packet_data["flag_array"], 0xFFED)) ~= 0 then  -- SYN+ACK = 0x12 = 18
+        tcp_sig_third = tcp_sig_third .. "F"
+    end
+
+    if tcp_sig_third == "" then
+        tcp_sig_third = "."
+    end
+
+    return tcp_sig_first .. tcp_sig_second .. tcp_sig_third
 end
 
 -- Function that tries to find a signature match between
@@ -113,8 +294,26 @@ function cgs_tcp_proto.dissector(buffer, pinfo, tree)
 
     -- Check relevant TCP flags
     local ack_check = osf_tcp_ack()
+    local ack_num = osf_tcp_ack_num()
     local fin_check = osf_tcp_fin()
     local syn_check = osf_tcp_syn()
+    local urg_check = osf_tcp_urgent()
+
+    -- Store other TCP/IP options
+    local tcp_wsize = osf_tcp_wsize()
+    local ip_ttl = osf_ip_ttl()
+    local ip_df_check = osf_ip_df()
+    local tcp_len = osf_tcp_len()
+    local tcp_mss = osf_tcp_mss()
+    local tcp_wscale = osf_tcp_wscale()
+
+    local ip_hdrlen = osf_ip_header_len()
+    local ip_id = osf_ip_id()
+    local ip_len = osf_ip_length()
+    local tcp_hdrlen = osf_tcp_header_len()
+
+    local tcp_flags = osf_tcp_flags()
+    local tcp_options = osf_tcp_options()
 
     -- We also store our dissection tree for TCP
     local tcp_tree = tree:add(cgs_tcp_proto, "OS FIngerprinting through TCP")
@@ -133,6 +332,9 @@ function cgs_tcp_proto.dissector(buffer, pinfo, tree)
             --print("Current Address:Port pairs: [" .. tostring(ip_src) .. ":" .. tostring(tcp_src) .. "], [" .. tostring(ip_dst) .. ":" .. tostring(tcp_dst) .. "]")
             local cur_stream_id = md5.sumhexa(tostring(ip_src) .. tostring(tcp_src) .. tostring(ip_dst) .. tostring(tcp_dst)) -- For consistency
             --print("Does ID = " .. cur_stream_id .. " exist?")
+            print("TCP Options = " .. tostring(inspect(tcp_options)))
+
+            local temp_tcp_sig = {}
 
             if cgs_tcp_stream_table[cur_stream_id] == nil then
                 -- Build a new entry in the stream table
@@ -151,6 +353,42 @@ function cgs_tcp_proto.dissector(buffer, pinfo, tree)
                     dst_port = tostring(tcp_dst)
                 }
 
+                -- After that, the next step is to build
+                -- our signature (in p0f format) and compare it
+                -- against the entries we have inside Satori's
+                -- fingerprint database:
+
+                print("WSIZE: " .. tostring(tcp_wsize) .. "; MSS: " .. tostring(tcp_mss) .. "; TTL: " .. tostring(ip_ttl) .. "; WSCALE: " .. tostring(tcp_wscale))
+
+                -- Signature build
+                temp_tcp_sig = {
+                    window_size = tonumber(tcp_wsize.value),
+                    df_bit = ip_df_check.value,
+                    syn_bit = syn_check.value,
+                    ack_bit = ack_check.value,
+                    ack_num = ack_num.value,
+                    urg_bit = urg_check.value,
+                    flag_array = tonumber(tcp_flags.value),
+                    tcp_header_len = tonumber(tcp_hdrlen.value),
+                    ip_header_len = tonumber(ip_hdrlen.value),
+                    ip_id = tonumber(ip_id.value),
+                    ip_length = tonumber(ip_len.value),
+                    packet_len = tonumber(tcp_len.value),
+                    mss = tonumber(tcp_mss.value),
+                    ttl = tonumber(ip_ttl.value),
+                    window_scale = tonumber(tcp_wscale.value),   -- In principle we'll only consider the exponent, not the scale itself
+                    options = tcp_options()
+                    -- Other options will be added later if they exist in the current packet
+                }
+
+                print("Current Partial TCP Signature = " .. inspect(temp_tcp_sig))
+
+                local str_sig = osf_tcp_dissector.osf_build_tcp_signature(temp_tcp_sig) -- p0fv2 format, as in Satori
+                print("Current TCP Signature = \"" .. str_sig .. "\"")
+
+                -- (...)
+
+                --- For debug purposes only ---
                 print(inspect(cgs_tcp_stream_table))
             else
                 -- If we get here, we just assume that
